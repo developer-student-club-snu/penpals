eda3e0b5a13886eeda4ec93fcd37cb6a230376ae
bhavenjain <61782538+bhavenjain@users.noreply.github.com>
2020-03-31 12:48:17 +0530

    Update Home.php

--- frontend/Home.php
+++ frontend/Home.php
@@ -5,57 +5,44 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?php echo __assets; ?>src/css/home.css">
+    <style>
+    </style>
 </head>
 <body>
     <div class="container">
 
         <div class="chat_list col-md-2 ">
             <div class="list_header">
-                <h3 id="greeting"></h3>
             </div>
             <div class="actions">
                 
-                <button class="start" id="start" data-toggle="modal" data-target="#info"><i class="fa fa-plus"></i></button>
+                <button class="start" id="start" data-toggle="modal" data-target="#modal_start">Start Conversation &nbsp;<i class="fa fa-chevron-right" style="font-size: 11px;"></i> </button>
             </div>
-                <button class="end"  data-toggle="modal" data-target="#logout" style="">LOGOUT</button>
-
-                <div id="logout" class="modal fade" role="dialog">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-body align">
-                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                    <label>Are you sure you want to logout? </label><br>
-                                    <button type="button" id="logout_btn" >Yes</button>
-                                    <button type="button" data-dismiss="modal">No</button>
-                                                                  
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-            
+                <button class="end" style="">LOGOUT</button>
         </div>
        
         <div class="convo_area col-md-10">
             <div class="convo_header" >
                 <div class="col-md-11" id="convo_header">
-                    <h1><b id="heading">USER</b></h1>
+                    <h1><b>USER</b></h1>
                 </div>
                 <div class = "col-md-1 parent dropdown" id="dropdown">
                     <i class="fa fa-ellipsis-v dropdown-toggle menu" data-toggle="dropdown"  aria-expanded="false"></i>
                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                         <a type="button" class="btn dropdown-dropdown-content1" data-toggle="modal" data-target="#modal_nickname">Set Nickname</a>
-                        <a type="button" id = "end" class="btn dropdown-dropdown-content1" data-toggle="modal" data-target="#info">End Conversation</a>     
+                        <a type="button" id = "end" class="btn dropdown-dropdown-content1" data-toggle="modal" data-target="#modal_end">End Conversation</a>     
                     </div>
-                    <div id="modal_nickname" class="modal fade" role="dialog">
+
+
+                    <div id="modal_nickname" style="top:35%;" class="modal fade" role="dialog">
                         <div class="modal-dialog">
                             <div class="modal-content">
                                 <div class="modal-body">
                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
                                 <form id = "nickname" >
-                                    <label>Set Nickname:</label>
-                                    <input name= "nick" placeholder = "nickname"></input>
-                                    <button type ="submit"  >Submit</button>
+                                    <label style="letter-spacing: 1px;">Set Nickname:</label>
+                                    <input name= "nick" class="nickname-text" placeholder = "Enter Nickname"></input>
+                                    <button type ="submit" class="submit_btn" style="">Submit</button>
                                 </form>
                                     
                                 </div>
@@ -63,31 +50,35 @@
 
                         </div>
                     </div>
-                </div>
 
-                    <div id="info" class="modal fade" role="dialog">
+                    <div id="modal_end" class="modal fade" style="top:35%;" role="dialog">
                         <div class="modal-dialog">
                             <div class="modal-content">
-                                <div class="modal-body align">
-                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <label id="waitlist"></label>
-                                    
+                                <div class="modal-body">                               
+                                <label id="waitlist"></label>                                
                                 </div>
                             </div>
 
                         </div>
                     </div>
-                    
-                
+                    <div id="modal_start" class="modal fade" style="top:35%;" role="dialog">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-body">                    
+                                <label id="waitlist"></label>                                    
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <div class="chat parent col-md-12" id="chat">
+            <div class="chat parent" id="chat">
                             
             </div>
-        
-        
-        
-        
+
+
             <form id="message_send">
                 <div class = "message_area">
                 
@@ -95,7 +86,7 @@
                         <textarea class="textbox" placeholder="Text" name="message_body"></textarea>
                     </div>
                     <div class="col-md-1 parent">
-                        <button class="send_btn" type = "submit"><img src="<?php echo __assets ?>image/send-icon.png" class="send_img"></button>
+                        <button class="send_btn" type = "submit"><img src="send-icon.png" class="send_img"></button>
                     </div>  
                 </div>
             </form>
